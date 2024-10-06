ff6fd1c889bbd370b67080001f46f8b8f6344936
Abdul Aljawaab <abdulkhamis4041@gmail.com>
2024-10-06 21:35:46 +0300

    coded_miniLister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form')// your code here
+  const toDosList = document.getElementById('tasks')
+  console.log(form, toDosList)
+
+  form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    const taskDescription = document.getElementById('new-task-description').value
+    const toDoListItem = document.createElement('li')
+    toDoListItem.textContent = taskDescription
+
+    toDosList.appendChild(toDoListItem)
+    
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = 'Delete'
+    deleteButton.classList.add('delete-button')
+
+    deleteButton.addEventListener('click', (event)=>{
+      toDoListItem.remove()
+    })
+
+    toDoListItem.appendChild(deleteButton)
+
+    toDosList.appendChild(toDoListItem)
+    
+    document.getElementById('new-task-description').value = ''
+       
+    
+  })
+
 });
